f5ac2d596f2631137546241d233fac977824b141
Daryl Findlay <darylfindlay@gmail.com>
2019-05-09 20:19:17 +0100

    Initial Commit

---
+++ Magic 8 Ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball
+//
+//  Created by Daryl Findlay on 09/05/2019.
+//  Copyright © 2019 Daryl Findlay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
